9482c4f81a8584c7315f97a6e8e26117b7e4e68b
kent0505 <otabekyusupov0550@gmail.com>
2024-12-02 23:17:03 +0500

    moved main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi              import FastAPI, Depends
+from fastapi              import FastAPI
 from fastapi.staticfiles  import StaticFiles
 
 from src.core.middlewares import setup_middlewares
