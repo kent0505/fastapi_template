f99a4c42192fb95e01af89f2d4317c519e2ffe6d
kent0505 <otabekyusupov0550@gmail.com>
2024-12-20 17:17:11 +0500

    2

--- src/bot/keyboards.py
+++ src/bot/keyboards.py
@@ -36,18 +36,7 @@ from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
 
 async def add_play_button():
     keyboard = InlineKeyboardBuilder()
-    keyboard.add(InlineKeyboardButton(
-        text="Play 1",
-        url="https://t.me/otvw_bot/test",
-    ))
-    keyboard.add(InlineKeyboardButton(
-        text="Play 2",
-        url="https://t.me/otvw_bot/test",
-    ))
-    keyboard.add(InlineKeyboardButton(
-        text="Play 3",
-        url="https://t.me/otvw_bot/test",
-    ))
+    keyboard.add(InlineKeyboardButton(text="Play", url="https://t.me/otvw_bot/test"))
     return keyboard.adjust(2).as_markup()
 
 
